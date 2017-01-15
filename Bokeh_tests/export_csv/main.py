09f56cff223832dd039046bad4b35e81547b5019
joshua-barber <j.barber501@gmail.com>
2017-01-15 14:24:10 +1100

    Setup project dream team

--- Bokeh_tests/export_csv/main.py
+++ Bokeh_tests/export_csv/main.py
@@ -1,43 +1,92 @@
-from os.path import dirname, join
-
 import pandas as pd
 
 from bokeh.layouts import row, widgetbox
-from bokeh.models import ColumnDataSource, CustomJS
-from bokeh.models.widgets import Slider, Button, DataTable, TableColumn, NumberFormatter
-from bokeh.io import curdoc
+from bokeh.models import Select
+from bokeh.palettes import Spectral5
+from bokeh.plotting import curdoc, figure
+from bokeh.sampledata.autompg import autompg
 
-df = pd.read_csv('salary_data.csv')
+df = autompg.copy()
 
-source = ColumnDataSource(data=dict())
+SIZES = list(range(6, 22, 3))
+COLORS = Spectral5
+ORIGINS = ['North America', 'Europe', 'Asia']
 
-def update():
-    current = df[df['salary'] <= slider.value].dropna()
-    source.data = {
-        'name'             : current.name,
-        'salary'           : current.salary,
-        'years_experience' : current.years_experience,
-    }
+# data cleanup
+df.cyl = [str(x) for x in df.cyl]
+df.origin = [ORIGINS[x-1] for x in df.origin]
 
-slider = Slider(title="Max Salary", start=10000, end=250000, value=150000, step=1000)
-slider.on_change('value', lambda attr, old, new: update())
+df['year'] = [str(x) for x in df.yr]
+del df['yr']
 
-button = Button(label="Download", button_type="success")
-button.callback = CustomJS(args=dict(source=source),
-                           code=open(join(dirname(__file__), "download.js")).read())
+df['mfr'] = [x.split()[0] for x in df.name]
+df.loc[df.mfr=='chevy', 'mfr'] = 'chevrolet'
+df.loc[df.mfr=='chevroelt', 'mfr'] = 'chevrolet'
+df.loc[df.mfr=='maxda', 'mfr'] = 'mazda'
+df.loc[df.mfr=='mercedes-benz', 'mfr'] = 'mercedes'
+df.loc[df.mfr=='toyouta', 'mfr'] = 'toyota'
+df.loc[df.mfr=='vokswagen', 'mfr'] = 'volkswagen'
+df.loc[df.mfr=='vw', 'mfr'] = 'volkswagen'
+del df['name']
 
-columns = [
-    TableColumn(field="name", title="Employee Name"),
-    TableColumn(field="salary", title="Income", formatter=NumberFormatter(format="$0,0.00")),
-    TableColumn(field="years_experience", title="Experience (years)")
-]
+columns = sorted(df.columns)
+discrete = [x for x in columns if df[x].dtype == object]
+continuous = [x for x in columns if x not in discrete]
+quantileable = [x for x in continuous if len(df[x].unique()) > 20]
 
-data_table = DataTable(source=source, columns=columns, width=800)
 
-controls = widgetbox(slider, button)
-table = widgetbox(data_table)
+def create_figure():
+    xs = df[x.value].values
+    ys = df[y.value].values
+    x_title = x.value.title()
+    y_title = y.value.title()
 
-curdoc().add_root(row(controls, table))
-curdoc().title = "Export CSV"
+    kw = dict()
+    if x.value in discrete:
+        kw['x_range'] = sorted(set(xs))
+    if y.value in discrete:
+        kw['y_range'] = sorted(set(ys))
+    kw['title'] = "%s vs %s" % (x_title, y_title)
 
-update()
+    p = figure(plot_height=600, plot_width=800, tools='pan,box_zoom,reset', **kw)
+    p.xaxis.axis_label = x_title
+    p.yaxis.axis_label = y_title
+
+    if x.value in discrete:
+        p.xaxis.major_label_orientation = pd.np.pi / 4
+
+    sz = 9
+    if size.value != 'None':
+        groups = pd.qcut(df[size.value].values, len(SIZES))
+        sz = [SIZES[xx] for xx in groups.codes]
+
+    c = "#31AADE"
+    if color.value != 'None':
+        groups = pd.qcut(df[color.value].values, len(COLORS))
+        c = [COLORS[xx] for xx in groups.codes]
+    p.circle(x=xs, y=ys, color=c, size=sz, line_color="white", alpha=0.6, hover_color='white', hover_alpha=0.5)
+
+    return p
+
+
+def update(attr, old, new):
+    layout.children[1] = create_figure()
+
+
+x = Select(title='X-Axis', value='mpg', options=columns)
+x.on_change('value', update)
+
+y = Select(title='Y-Axis', value='hp', options=columns)
+y.on_change('value', update)
+
+size = Select(title='Size', value='None', options=['None'] + quantileable)
+size.on_change('value', update)
+
+color = Select(title='Color', value='None', options=['None'] + quantileable)
+color.on_change('value', update)
+
+controls = widgetbox([x, y, color, size], width=200)
+layout = row(controls, create_figure())
+
+curdoc().add_root(layout)
+curdoc().title = "Crossfilter"
